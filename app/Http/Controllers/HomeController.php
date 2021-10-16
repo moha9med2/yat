ecd3ef49d341299b3e8b965321d0976e0851e962
unknown <moha9medsaeed@gmail.com>
2021-10-16 18:03:49 +0200

    fix

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function contact(){
+
+        $arr = 'ali, ahmed, gfdg d,dfg dfg fd';
+        $names = explode(',',$arr);
+
+        $n = $names[0];
+
+        foreach($names as $name){
+            $name;
+        }
+        
+       
+        return view('contact',compact('name','arr','n'));
+    }
+
+    public function about(){
+
+        $category =  Category::all();
+
+
+        return view('about',compact('category'));
+
+    }
+}
